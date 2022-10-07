91685be522cfc8eb1a66bb9ae5b0ec5cdf051c1d
hawary-id <hawaryproject@gmail.com>
2022-10-08 03:55:41 +0700

    feat: admin update movie

--- resources/js/Pages/Admin/Movie/Index.jsx
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -1,11 +1,12 @@
 import Button from "@/Components/Button";
 import FlashMessage from "@/Components/FlashMessage";
 import Authenticated from "@/Layouts/Authenticated/Index";
-import { Link } from "@inertiajs/inertia-react";
+import { Head, Link } from "@inertiajs/inertia-react";
 
-export default function Index({ auth, flashMessage }) {
+export default function Index({ auth, flashMessage, movies }) {
     return (
         <Authenticated auth={auth}>
+            <Head title="Admin - List Movie" />
             <Link href={route("admin.dashboard.movie.create")}>
                 <Button type="button" className="w-40 mb-8">
                     Insert new movie
@@ -14,6 +15,58 @@ export default function Index({ auth, flashMessage }) {
             {flashMessage?.message && (
                 <FlashMessage message={flashMessage.message} />
             )}
+
+            <table className="table-fixed w-full text-center">
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Category</th>
+                        <th>Rating</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((movie) => (
+                        <tr key={movie.id}>
+                            <td>
+                                <img
+                                    src={`/storage/${movie.thumbnail}`}
+                                    alt=""
+                                    className="rounded-md"
+                                />
+                            </td>
+                            <td>{movie.name}</td>
+                            <td>{movie.category}</td>
+                            <td>{movie.rating.toFixed(1)}</td>
+                            <td className="flex flex-row gap-2">
+                                <Link
+                                    className="basis-1/2"
+                                    href={route(
+                                        "admin.dashboard.movie.edit",
+                                        movie.id
+                                    )}
+                                >
+                                    <Button type="button" variant="warning">
+                                        Edit
+                                    </Button>
+                                </Link>
+                                <Link
+                                    className="basis-1/2"
+                                    href={route(
+                                        "admin.dashboard.movie.edit",
+                                        movie.id
+                                    )}
+                                >
+                                    <Button type="button" variant="danger">
+                                        Edit
+                                    </Button>
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </Authenticated>
     );
 }
